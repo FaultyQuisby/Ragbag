0b0b0b42bb9e422c0de1e9bca127ba4f0dc566f0
FaultyQuisby <faultyquisby@protonmail.com>
2019-05-14 16:13:03 +0200

    added path to file in .vimrc and fixed list in library

--- c-skeleton/tests/minunit.h
+++ c-skeleton/tests/minunit.h
@@ -3,27 +3,27 @@
 #define _minunit_h
 
 #include <stdio.h>
-#include "lcthw/dbg.h"
+#include "../src/lcthw/dbg.h"
 #include <stdlib.h>
 
 #define mu_suite_start() char *message = NULL
 
 #define mu_assert(test, message) if (!(test)) { log_err(message); return message; }
-#define mu_run_test(test) debug("\n-----%s", " " #test); \
+#define mu_run_test(test);  debug("-----%s", " " #test);\
     message = test(); tests_run++; if (message) return message;
 
 #define RUN_TESTS(name) int main(int argc, char *argv[]) {\
     argc = 1; \
-    debug("----- RUNNING: %s", argv[0]);\
-    printf("----\nRUNNING: %s\n", argv[0]);\
+    debug("\n-----\nRUNNING: ./%s", argv[0]);\
     char *result = name();\
     if (result != 0) {\
-        printf("FAILED: %s\n", result);\
+        debug("FAILED: %s", result);\
     }\
     else {\
-        printf("ALL TESTS PASSED\n");\
+        debug("ALL TESTS PASSED");\
     }\
-    printf("Tests run: %d\n", tests_run);\
+    debug("Tests run: %d", tests_run);\
+    debug("./%s PASS", argv[0]);\
     exit(result != 0);\
 }
 
